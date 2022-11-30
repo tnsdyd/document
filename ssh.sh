6083a15861125a11e1af14beb22fd7f34e0c612a
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-30 16:10:40 +0900

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST='sshpass -p cmk476357! scp README.md Administrator@43.201.68.155:c:/'
-TEST2='sshpass -p cmk476357! ssh README.md Administrator@43.201.68.155'
+TEST2='sshpass -p cmk476357! ssh -o StrictHostKeyChecking=no Administrator@43.201.68.155'
 
 
 $TEST2 dir
