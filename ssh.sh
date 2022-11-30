7f592169f945af453fb7e9a65f633a9ca89a4d6b
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-30 16:50:16 +0900

    Delete ssh.sh

--- ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TEST='sshpass -p cmk476357! scp README.md Administrator@43.201.68.155:c:/'
-TEST2='sshpass -p cmk476357! ssh -o StrictHostKeyChecking=no Administrator@43.201.68.155'
-
-
-$TEST2 dir
-$TEST
